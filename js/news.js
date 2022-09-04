8bf0349736691a0e9d336c864410ccdc76929d6c
DevMazed04 <mdmazedislam2684@gmail.com>
2022-09-04 23:50:14 +0600

    assignment six completed...

--- js/news.js
+++ js/news.js
@@ -10,15 +10,13 @@ const loadAllCategories = () => {
 
 /* ============ display all news category ============ */
 const displayAllCategories = (categories) => {
-  //console.log(categories);
   const categoriesNavbar = document.getElementById("categories-navbar");
   categories.forEach((category) => {
-    //console.log(category);
     const ulCategory = document.createElement("ul");
     ulCategory.classList.add("navbar-nav", "mx-auto", "mb-2", "mb-lg-0");
     ulCategory.innerHTML = `
-        <li class="nav-item px-3">
-        <a class="nav-link" aria-current="page" onclick="loadNewsInACategory('${category.category_id}', '${category.category_name}')"
+        <li class="nav-item px-3 category">
+        <a role="button" class="nav-link" aria-current="page" onclick="loadNewsInACategory('${category.category_id}', '${category.category_name}')"
         > ${category.category_name}</a>
         </li>
         `;
@@ -50,18 +48,16 @@ loadNewsInACategory("03", "International News");
 const displayNewsInACategory = (allNews, categoryName) => {
   // sorting news by total view
   allNews.sort((a, b) => b.total_view - a.total_view);
-  //console.log(allNews);
 
   const newsContainer = document.getElementById("news-in-category");
   newsContainer.textContent = "";
   allNews.forEach((news) => {
-    //console.log(news);
     const newsDiv = document.createElement("div");
     newsDiv.classList.add("col");
     newsDiv.innerHTML = `
-      <div class="container card mb-3" >
+      <div class="container card mb-3 p-3" >
         <div class="row g-0">
-          <div class="col-md-4 mt-2">
+          <div class="col-md-4">
             <img
               src="${news.image_url}"
               class="img-fluid rounded-start"
@@ -70,12 +66,15 @@ const displayNewsInACategory = (allNews, categoryName) => {
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">${news.title}</h5>
+              <h5 class="card-title">${news.title.length > 70
+        ? news.title.slice(0, 70) + "..."
+        : news.title
+      }</h5>
               <p class="card-text">
-                ${news.details.length > 200
-                ? news.details.slice(0, 200) + "..."
-                : news.details
-              }
+                ${news.details.length > 165
+        ? news.details.slice(0, 165) + "..."
+        : news.details
+      }
               </p>
               <div class="d-flex justify-content-between align-items-center">
               <div class="d-flex gap-2 align-items-center">
@@ -87,12 +86,14 @@ const displayNewsInACategory = (allNews, categoryName) => {
                   </div>
                 </div>
                 <div class="mt-3">
-                  <p class="fw-semibold text-md">${news.author.name}</p>
+                  <p class="fw-semibold text-md">${news.author.name ? news.author.name : "No author found"
+      }</p>
                 </div>
               </div>
               <div class="d-flex gap-2 mt-3">
                 <p><i class="fa-solid fa-eye"></i></p>
-                <p class="fw-bold">${news.total_view}</p>
+                <p class="fw-bold">${news.total_view ? news.total_view : "No views"
+      }</p>
               </div>
               <div class="d-none d-sm-block">
                 <i class="fa-solid fa-star text-warning"></i>
@@ -102,7 +103,7 @@ const displayNewsInACategory = (allNews, categoryName) => {
                 <i class="fa-regular fa-star text-warning"></i>
               </div>
               <button class="btn btn-sm btn-outline-primary" data-bs-toggle="modal" data-bs-target="#newsDetailsModal" onclick="loadNewsDetails('${news._id
-              }')">Details
+      }')">Details
               </button>
              </div>
             </div>
@@ -134,18 +135,23 @@ const loadNewsDetails = (newsId) => {
 };
 
 const displayNewsDetails = (newsDetails) => {
-  //console.log(newsDetails);
   const newsDetailsModal = document.getElementById("newsDetailsModalLabel");
   newsDetailsModal.innerText = newsDetails.title;
   const newsAuthor = document.getElementById("news-details");
   newsAuthor.innerHTML = `
   <div id="news-details" class="modal-body">
     <img class="img-fluid mb-4" src="${newsDetails.image_url}" alt="">
-    <p><b>Published Date:</b> ${newsDetails.author.published_date} </p>
-    <p><b>Author Name:</b> ${newsDetails.author.name} </p>
-    <p><b>Total Views:</b> ${newsDetails.total_view}</p>
-    <p><b>Ratings:</b> ${newsDetails.rating.number} </p>
-    <p><b>Description:</b> ${newsDetails.details}</p>
+    <p><b>Published Date:</b> ${newsDetails.author.published_date
+      ? newsDetails.author.published_date
+      : "No published date found"
+    } </p>
+    <p><b>Author Name:</b> ${newsDetails.author.name ? newsDetails.author.name : "No author found"
+    } </p>
+    <p><b>Total Views:</b> ${newsDetails.total_view ? newsDetails.total_view : "No views"
+    }</p>
+    <p><b>Ratings:</b> ${newsDetails.rating.number ? newsDetails.rating.number : "No ratings found"
+    } </p>
+    <p class="mb-0"><b>Description:</b> ${newsDetails.details}</p>
 </div>
   `;
 };
